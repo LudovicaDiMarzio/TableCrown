e7982ccbb14a6be0804bcd6229122ce016d34a74
Marco <damicomarco04@gmail.com>
2026-06-11 17:49:42 +0200

    sto impazzendo

--- test_preview.php
+++ test_preview.php
@@ -3,22 +3,31 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Smarty\Smarty;
 $smarty = new Smarty();
-// Sostituito 'Presentation' con 'presentation'
-$templatedir = __DIR__ . '/presentation/view_smarty_dir/templates/';
-$compiledir  = __DIR__ . '/presentation/view_smarty_dir/templates_c/';
-$cachedir    = __DIR__ . '/presentation/view_smarty_dir/cache/';
-$configdir   = __DIR__ . '/presentation/view_smarty_dir/configs/';
-// DEBUG REALE: Adesso stamperà la cartella corretta senza Warning
-if (!is_dir($templatedir)) {
-    echo "<strong style='color:red;'>ERRORE DI PERCORSO!</strong><br>";
-    echo "Smarty sta cercando la cartella qui: <br><code>" . htmlspecialchars($templatedir) . "</code><br><br>";
-    echo "<strong>Cosa controllare adesso:</strong><br>";
-    echo "1. Controlla se la cartella 'Presentation' ha la 'P' maiuscola o minuscola nel tuo computer.<br>";
-    echo "2. Controlla se la cartella si chiama esattamente 'view_smarty_dir'.<br>";
+
+// Usiamo realpath per azzerare i problemi di slash, maiuscole e minuscole di Windows
+$basePath = realpath(__DIR__);
+
+// Proviamo a cercare la cartella provando sia la P maiuscola che minuscola automaticamente
+$subFolder = '/Presentation/view_smarty_dir';
+if (!is_dir($basePath . $subFolder)) {
+    $subFolder = '/presentation/view_smarty_dir'; // ripiego su minuscolo
+}
+
+$templatedir = realpath($basePath . $subFolder . '/templates');
+$compiledir  = realpath($basePath . $subFolder . '/templates_c');
+$cachedir    = realpath($basePath . $subFolder . '/cache');
+$configdir   = realpath($basePath . $subFolder . '/configs');
+
+// DEBUG DEFINITIVO
+if (!$templatedir || !is_dir($templatedir)) {
+    echo "<strong style='color:red;'>DIAGNOSTICA PERCORSI fallita!</strong><br><br>";
+    echo "La root del progetto è rilevata come: <code>" . htmlspecialchars($basePath) . "</code><br>";
+    echo "Sto cercando di entrare in una cartella chiamata <code>presentation</code> o <code>Presentation</code> che contenga al suo interno <code>view_smarty_dir</code>.<br><br>";
+    echo "<strong>Cosa fare ora:</strong> Apri la cartella <code>TableCrown</code> su Windows e controlla se i nomi di queste cartelle sono scritti esattamente così, o se per caso hai inserito <code>view_smarty_dir</code> dentro qualcos'altro (es. dentro <code>src</code> o direttamente nella root).";
     exit;
 }
 
-// Se la cartella esiste, impostiamo i percorsi in Smarty
+// Se i percorsi sono validi, li passiamo a Smarty
 $smarty->setTemplateDir($templatedir);
 $smarty->setCompileDir($compiledir);
 $smarty->setCacheDir($cachedir);
