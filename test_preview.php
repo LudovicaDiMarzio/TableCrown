5a830d56ea5c8df7b304c5701034c891b7292540
Marco <damicomarco04@gmail.com>
2026-06-11 17:46:27 +0200

    debuging va meglio ma non bene comunque

--- test_preview.php
+++ test_preview.php
@@ -12,8 +12,21 @@ $smarty->setCompileDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secon
 $smarty->setCacheDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/Presentation/smarty-dir/cache/');
 $smarty->setConfigDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/Presentation/smarty-dir/configs/');
 
-// Se nel layout usi delle variabili (es. il titolo della pagina), puoi simularle qui
+// 2. DEBUG TEMPORANEO: Controlliamo se Windows vede davvero questa cartella
+if (!is_dir($templateDir)) {
+    echo "<strong style='color:red;'>ERRORE DI PERCORSO!</strong><br>";
+    echo "Smarty sta cercando i template qui: <br><code>" . htmlspecialchars($templateDir) . "</code><br>";
+    echo "Ma questa cartella NON esiste. Controlla se hai scritto bene le maiuscole o se si chiama 'view_smarty_dir'.";
+    exit;
+}
+
+// 3. Se la cartella esiste, la impostiamo in Smarty
+$smarty->setTemplateDir($templateDir);
+$smarty->setCompileDir($compileDir);
+$smarty->setCacheDir($cacheDir);
+$smarty->setConfigDir($configDir);
+
 $smarty->assign('page_title', 'Anteprima Layout');
 
-// Mostra il layout (o il template specifico che include il layout)
+// 4. Mostra il template
 $smarty->display('common/layout.tpl');
