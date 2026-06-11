2210914654d9b4d86dc657c01e3e6d23cffa2da0
Marco <damicomarco04@gmail.com>
2026-06-11 17:40:13 +0200

    tryal and error per i test

--- test_preview.php
+++ test_preview.php
@@ -1,14 +1,14 @@
 <?php
 // Includi l'autoloader di Composer o Smarty direttamente
-require_once __DIR__ . '/../vendor/autoload.php'; // Aggiusta il path in base a dove si trova
+require_once 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/vendor/autoload.php';
 
 $smarty = new Smarty();
 
 // Configura i percorsi delle cartelle che hai creato
-$smarty->setTemplateDir(__DIR__ . '/presentation/view_smarty_dir/templates/');
-$smarty->setCompileDir(__DIR__ . '/presentation/view_smarty_dir/templates_c/');
-$smarty->setCacheDir(__DIR__ . '/presentation/view_smarty_dir/cache/');
-$smarty->setConfigDir(__DIR__ . '/presentation/view_smarty_dir/configs/');
+$smarty->setTemplateDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/Presentation/smarty-dir/templates/');
+$smarty->setCompileDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/Presentation/smarty-dir/templates_c/');
+$smarty->setCacheDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/Presentation/smarty-dir/cache/');
+$smarty->setConfigDir(__DIR__ . 'C:/Users/damic/Desktop/uni/APPUNTI/anno3/secondo_semenstre/Pweb/TableCrown/Presentation/smarty-dir/configs/');
 
 // Se nel layout usi delle variabili (es. il titolo della pagina), puoi simularle qui
 $smarty->assign('page_title', 'Anteprima Layout');
