0837ebcdc1d7c789a97579ffeb5ef6cf23a9c65d
Marco <damicomarco04@gmail.com>
2026-06-13 10:23:29 +0200

    Ecarrello item Docrtrine

--- Entity/ECarrelloItem.php
+++ Entity/ECarrelloItem.php
@@ -1,56 +1,61 @@
 <?php
 namespace TableCrown\Entity;
- 
+
 use InvalidArgumentException;
- 
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: "carrello_item")]
 class ECarrelloItem {
-    private int $idCarrelloItem;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: "integer")]
+    private ?int $idCarrelloItem = null;
+
+    #[ORM\Column(type: "integer")]
     private int $quantita;
-    private int $idCarrello; // FK -> ECarrello
-    private int $idProdotto; // FK -> EProdotto
- 
-    public function __construct(int $idCarrelloItem, int $quantita, int $idCarrello, int $idProdotto) {
-        $this->idCarrelloItem = $idCarrelloItem;
- 
+
+    #[ORM\ManyToOne(targetEntity: ECarrello::class, inversedBy: "carrelloItems")]
+    #[ORM\JoinColumn(name: "carrello_id", referencedColumnName: "idCarrello", nullable: false)]
+    private ECarrello $carrello;
+
+    #[ORM\ManyToOne(targetEntity: EProdotto::class)]
+    #[ORM\JoinColumn(name: "prodotto_id", referencedColumnName: "idprodotto", nullable: false)]
+    private EProdotto $prodotto;
+
+    public function __construct(int $quantita, ECarrello $carrello, EProdotto $prodotto) {
+        $this->impostaQuantita($quantita);
+        $this->carrello = $carrello;
+        $this->prodotto = $prodotto;
+    }
+
+    // Metodi di dominio
+    public function impostaQuantita(int $quantita): void {
         if ($quantita <= 0) {
             throw new InvalidArgumentException("La quantità deve essere maggiore di 0.");
         }
         $this->quantita = $quantita;
- 
-        $this->idCarrello = $idCarrello;
-        $this->idProdotto = $idProdotto;
     }
- 
-    // SET methods
-    public function setQuantita(int $quantita): void {
-        if ($quantita <= 0) {
-            throw new InvalidArgumentException("La quantità deve essere maggiore di 0.");
-        }
-        $this->quantita = $quantita;
+
+    public function incrementaQuantita(int $valore = 1): void {
+        $this->impostaQuantita($this->quantita + $valore);
     }
- 
-    public function setIdCarrello(int $idCarrello): void {
-        $this->idCarrello = $idCarrello;
-    }
- 
-    public function setIdProdotto(int $idProdotto): void {
-        $this->idProdotto = $idProdotto;
-    }
- 
+
     // GET methods
-    public function getIdCarrelloItem(): int {
+    public function getIdCarrelloItem(): ?int {
         return $this->idCarrelloItem;
     }
- 
+
     public function getQuantita(): int {
         return $this->quantita;
     }
- 
-    public function getIdCarrello(): int {
-        return $this->idCarrello;
+
+    public function getCarrello(): ECarrello {
+        return $this->carrello;
     }
- 
-    public function getIdProdotto(): int {
-        return $this->idProdotto;
+
+    public function getProdotto(): EProdotto {
+        return $this->prodotto;
     }
 }
