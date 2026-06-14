93f08c8c57ca0fca4cfcb9028d2e6c275aeceebd
laurabadia22 <laurabadia22@gmail.com>
2026-06-14 13:48:01 +0200

    creazione dei file di configurazione per la connessione e la creazione del db , bootstrap.php e create_db.php

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,6 @@
 <?php
-//configura la connessione al db
-require_once __DIR__ . '/vendor/autoload.php';
+//carica l'autoloader di composer per trovare le librerie e le classi entity
+require_once __DIR__ . '/vendor/autoload.php'; //__DIR__ restituisce il percorso assoluto del file che stiamo leggendo (/vendor/autoload.php)
 
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
@@ -8,13 +8,22 @@ use Doctrine\DBAL\DriverManager;
 
 function getEntityManager(): EntityManager
 {
+    //applico il pattern singleton per evitare di creare più di un'istanza di entitymanager
+    static $entityManager = null;
+
+    if ($entityManager !== null) {
+        return $entityManager;
+    }
+
+    //metodo di doctrine per creare la configurazione del db basandosi sugli attributi doctrine che abbiamo messo nelle entity (traduttore delle classi da php a doctrine)
     $config = ORMSetup::createAttributeMetadataConfiguration(
-        paths: [__DIR__ . '/Entity'],  // cartella entità
-        isDevMode: true
+        paths: [__DIR__ . '/Entity'],  //comunico a doctrine dove sono le classi mappate
+        isDevMode: true //siamo ancora in modalità sviluppo, quindi ogni volta che facciamo partire una pagina, doctrine si rilegge le entity da zero
     );
 
+    //configurazione dati di accesso per la connessione al db
     $conn = [
-        'driver'   => 'pdo_mysql',
+        'driver'   => 'pdo_mysql', //tipologia di db utilizzato (MySQL)
         'host'     => 'localhost',
         'dbname'   => 'tablecrown',  // nome db
         'user'     => 'root',
@@ -22,6 +31,12 @@ function getEntityManager(): EntityManager
         'charset'  => 'utf8'
     ];
 
+    //metodo doctrine per creare la connessione al db dati i parametri di configurazione precedenti
     $connection = DriverManager::getConnection($conn, $config);
-    return  new EntityManager($connection, $config);
+
+    //creo l'entity manager che comunicherà con il db attraverso la connessione creata, sfruttando anche la configurazione doctrine che abbiamo scritto nelle entity 
+    //EntityManagaer è una classe Doctrine 
+    $entityManager = new EntityManager($connection, $config);
+
+    return $entityManager;
 }
--- create_db.php
+++ create_db.php
@@ -3,13 +3,34 @@
 require_once 'bootstrap.php';
 
 use Doctrine\ORM\Tools\SchemaTool;
+use TableCrown\Entity\EPersona;
 use TableCrown\Entity\EUtente;
 use TableCrown\Entity\EAmministratore;
 use TableCrown\Entity\ESegnalazione;
 use TableCrown\Entity\EMotivazione;
 use TableCrown\Entity\EProvvedimento;
-// aggiungi tutte le altre entità
+use TableCrown\Entity\EBustine;
+use TableCrown\Entity\ECarrello;
+use TableCrown\Entity\ECarrelloItem;
+use TableCrown\Entity\ECartaDiCredito;
+use TableCrown\Entity\EChallenge;
+use TableCrown\Entity\EDanno;
+use TableCrown\Entity\EEvento;
+use TableCrown\Entity\EGestore;
+use TableCrown\Entity\EGiocoDaTavolo;
+use TableCrown\Entity\EIndirizzo;
+use TableCrown\Entity\EOrdine;
+use TableCrown\Entity\EOrdineItem;
+use TableCrown\Entity\EPartecipazione;
+use TableCrown\Entity\EPortaDadi;
+use TableCrown\Entity\EPrezzo;
+use TableCrown\Entity\EProdotto;
+use TableCrown\Entity\ERecensione;
+use TableCrown\Entity\ETorneo;
+use TableCrown\Entity\ESerata;
+use TableCrown\Entity\EWishlist;
 
+//creo l'entity manager sfruttando il codice di bootstrap.php
 $entityManager = getEntityManager();
 
 // Gestisce gli enum come stringhe
@@ -18,22 +39,45 @@ if (!$platform->hasDoctrineTypeMappingFor('enum')) {
     $platform->registerDoctrineTypeMapping('enum', 'string');
 }
 
+//per aggirare i sistemi di sicurezza dei db, altrimenti non potrei ricostruire il db da capo ogni volta
 $entityManager->getConnection()->executeStatement('SET FOREIGN_KEY_CHECKS = 0');
 
 $schemaTool = new SchemaTool($entityManager);
 
+//elenco di tutte le classi che volgio trasformare in tabelle
 $classes = [
     $entityManager->getClassMetadata(EUtente::class),
     $entityManager->getClassMetadata(EAmministratore::class),
     $entityManager->getClassMetadata(ESegnalazione::class),
     $entityManager->getClassMetadata(EMotivazione::class),
     $entityManager->getClassMetadata(EProvvedimento::class),
-    // aggiungi tutte le altre entità
+    $entityManager->getClassMetadata(EBustine::class),
+    $entityManager->getClassMetadata(ECarrello::class),
+    $entityManager->getClassMetadata(ECarrelloItem::class),
+    $entityManager->getClassMetadata(ECartaDiCredito::class),
+    $entityManager->getClassMetadata(EChallenge::class),
+    $entityManager->getClassMetadata(EDanno::class),
+    $entityManager->getClassMetadata(EEvento::class),
+    $entityManager->getClassMetadata(EGestore::class),
+    $entityManager->getClassMetadata(EGiocoDaTavolo::class),
+    $entityManager->getClassMetadata(EIndirizzo::class),
+    $entityManager->getClassMetadata(EOrdine::class),
+    $entityManager->getClassMetadata(EOrdineItem::class),
+    $entityManager->getClassMetadata(EPartecipazione::class),
+    $entityManager->getClassMetadata(EPersona::class),
+    $entityManager->getClassMetadata(EPortaDadi::class),
+    $entityManager->getClassMetadata(EPrezzo::class),
+    $entityManager->getClassMetadata(EProdotto::class),
+    $entityManager->getClassMetadata(ERecensione::class),
+    $entityManager->getClassMetadata(ESerata::class),
+    $entityManager->getClassMetadata(ETorneo::class),
+    $entityManager->getClassMetadata(EWishlist::class)
 ];
 
-$schemaTool->dropDatabase();   // cancella il db esistente
-$schemaTool->createSchema($classes);  // crea le tabelle
+$schemaTool->dropDatabase();   // cancella le vecchie tabelle, se esistenti
+$schemaTool->createSchema($classes);  // crea le tabelle del db
 
+//creazione del db
 $entityManager->getConnection()->executeStatement('SET FOREIGN_KEY_CHECKS = 1');
 
 echo "Database creato con successo!\n";
