b9697f85f11b75b1d1454e26d1b9fad3a3c9da2e
LudovicaDiMarzio <ludovicadm2004@gmail.com>
2026-07-04 12:47:04 +0200

    Merge di classi enumerative LinguaGioco e DifficoltaGioco da sviluppo_entity.

---
+++ Entity/Enumerativi/DifficoltaGioco.php
@@ -0,0 +1,9 @@
+<?php
+namespace TableCrown\Entity\Enumerativi;
+
+enum DifficoltaGioco: string {
+    case Facile = "facile";
+    case Media = "media";
+    case Difficile = "difficile";
+    case Esperto = "esperto";
+}
