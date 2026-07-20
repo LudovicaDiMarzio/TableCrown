2356a9a4968180f7211797850d787b100b0b40c4
Marco <damicomarco04@gmail.com>
2026-07-20 15:48:15 +0200

    file dimenticato

---
+++ Presentation/Views/ViewGestoreCreazioneChallenge.php
@@ -0,0 +1,9 @@
+<?php
+namespace TableCrown\Presentation\Views;
+
+class ViewGestoreCreazioneChallenge extends BaseViewGestore {
+
+    protected function getTemplateName(array $dati): string {
+        return 'gestore_creazione_challenge.tpl';
+    }
+}
