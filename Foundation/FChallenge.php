6a40dd115bfc7a00eccb1b3b79852330412390f8
laurabadia22 <laurabadia22@gmail.com>
2026-07-20 11:31:31 +0200

    integrazione metodi per gestore

--- Foundation/FChallenge.php
+++ Foundation/FChallenge.php
@@ -34,4 +34,31 @@ class FChallenge{
             return [];
         }
     }
+
+
+    /**
+     * @param string $filtroData data di inizio del filtro
+     * @return array di oggetti
+     * @throws Exception
+     */
+    public static function findChallengeGestore(?string $filtroData): array{
+        try{
+            $qb=FEntityManager::getInstance()->getEntityManager()->createQueryBuilder();
+            $qb->select('c')
+                ->from(EChallenge::class, 'c');
+            if($filtroData!==null){
+                $dataObj = new \DateTime($filtroData);
+                $qb->andWhere('c.dataInizio>=:dataEvento')
+                    ->setParameter('dataEvento', $dataObj);
+            }
+        
+            $qb->orderBy('c.dataInizio', 'ASC');
+            $risultati = $qb->getQuery()->getResult();
+            return $risultati;
+        }
+        catch(Exception $e){
+            error_log("Errore in findChallenge: " . $e->getMessage());
+            return [];
+        }
+    }
 }
