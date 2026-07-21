538f8104726915883f606a7a29e2a4e8071b41cf
Marco <damicomarco04@gmail.com>
2026-07-21 10:52:20 +0200

    aggiornamento per serate e per i breadcrumbs

--- Presentation/smarty-dir/templates_c/2e242b2041184fd4a5af5585378b157b229b3099_0.file_dettagliSerata.tpl.php
+++ Presentation/smarty-dir/templates_c/2e242b2041184fd4a5af5585378b157b229b3099_0.file_dettagliSerata.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.8.0, created on 2026-07-20 21:03:26
+/* Smarty version 5.8.0, created on 2026-07-21 10:22:33
   from 'file:dettagliSerata.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.8.0',
-  'unifunc' => 'content_6a5e70fe880859_30110523',
+  'unifunc' => 'content_6a5f2c49c3b724_28924424',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2e242b2041184fd4a5af5585378b157b229b3099' => 
     array (
       0 => 'dettagliSerata.tpl',
-      1 => 1784574201,
+      1 => 1784622149,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_6a5e70fe880859_30110523 (\Smarty\Template $_smarty_tpl) {
+function content_6a5f2c49c3b724_28924424 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'C:\\xampp\\htdocs\\TableCrown\\Presentation\\smarty-dir\\templates';
 $_smarty_tpl->getInheritance()->init($_smarty_tpl, true);
 ?>
@@ -28,11 +28,11 @@ $_smarty_tpl->getInheritance()->init($_smarty_tpl, true);
 
 
 <?php 
-$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_14698608026a5e70fe86b624_44987964', "content");
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_19983782846a5f2c49c265c6_64199087', "content");
 $_smarty_tpl->getInheritance()->endChild($_smarty_tpl, "common/layout.tpl", $_smarty_current_dir);
 }
 /* {block "content"} */
-class Block_14698608026a5e70fe86b624_44987964 extends \Smarty\Runtime\Block
+class Block_19983782846a5f2c49c265c6_64199087 extends \Smarty\Runtime\Block
 {
 public function callBlock(\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'C:\\xampp\\htdocs\\TableCrown\\Presentation\\smarty-dir\\templates';
@@ -43,8 +43,6 @@ $_smarty_current_dir = 'C:\\xampp\\htdocs\\TableCrown\\Presentation\\smarty-dir\
 
 <div class="dettaglio-container">
     <div class="container">
-
-        
 
         <!-- ── BLOCCO SUPERIORE (senza box prezzo) ── -->
         <div class="dettaglio-top">
@@ -95,28 +93,33 @@ echo ($_smarty_tpl->getValue('serata')['postiLiberi']/$_smarty_tpl->getValue('se
                     </div>
                 </div>
 
-                <div class="dettaglio-azione-card">
-                    <span class="dettaglio-azione-label">Quanti posti vuoi prenotare?</span>
-                    <div class="dettaglio-azione-row">
-                        <div class="dettaglio-qty" id="qty-prenota">
-                            <button type="button" class="dettaglio-qty-btn" id="btn-qty-minus"
-                                    <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>disabled<?php }?>>−</button>
-                            <input type="number" class="dettaglio-qty-input" id="input-qty-posti"
-                                   value="1" min="1" max="<?php echo $_smarty_tpl->getValue('serata')['postiLiberi'];?>
-" readonly>
-                            <button type="button" class="dettaglio-qty-btn" id="btn-qty-plus"
-                                    <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>disabled<?php }?>>+</button>
-                        </div>
-                        <a href="#recensioni" class="dettaglio-recensioni-link" style="display:none;"></a>
-                    </div>
+                <form method="POST" action="<?php echo $_smarty_tpl->getValue('base_url');?>
+/eventi/partecipa" id="form-prenota">
+                    <input type="hidden" name="id_evento" value="<?php echo $_smarty_tpl->getValue('serata')['id'];?>
+">
 
-                    <button type="button" class="btn-partecipa" data-id="<?php echo $_smarty_tpl->getValue('serata')['id'];?>
-" id="btn-prenota"
-                            <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>disabled<?php }?>>
-                        <i class="ti ti-calendar-check"></i>
-                        <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>Posti esauriti<?php } else { ?>Prenota<?php }?>
-                    </button>
-                </div>
+                    <div class="dettaglio-azione-card">
+                        <span class="dettaglio-azione-label">Quanti posti vuoi prenotare?</span>
+                        <div class="dettaglio-azione-row">
+                            <div class="dettaglio-qty" id="qty-prenota">
+                                <button type="button" class="dettaglio-qty-btn" id="btn-qty-minus"
+                                        <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>disabled<?php }?>>−</button>
+                                <input type="number" class="dettaglio-qty-input" id="input-qty-posti"
+                                       name="posti" value="1" min="1" max="<?php echo $_smarty_tpl->getValue('serata')['postiLiberi'];?>
+" readonly>
+                                <button type="button" class="dettaglio-qty-btn" id="btn-qty-plus"
+                                        <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>disabled<?php }?>>+</button>
+                            </div>
+                            <a href="#recensioni" class="dettaglio-recensioni-link" style="display:none;"></a>
+                        </div>
+
+                        <button type="submit" class="btn-partecipa" id="btn-prenota"
+                                <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>disabled<?php }?>>
+                            <i class="ti ti-calendar-check"></i>
+                            <?php if ($_smarty_tpl->getValue('serata')['postiLiberi'] <= 0) {?>Posti esauriti<?php } else { ?>Prenota<?php }?>
+                        </button>
+                    </div>
+                </form>
             </div>
 
         </div>
@@ -139,7 +142,6 @@ echo ($_smarty_tpl->getValue('serata')['postiLiberi']/$_smarty_tpl->getValue('se
     const input = document.getElementById('input-qty-posti');
     const btnMinus = document.getElementById('btn-qty-minus');
     const btnPlus = document.getElementById('btn-qty-plus');
-    const btnPrenota = document.getElementById('btn-prenota');
 
     function clamp(val) {
         if (val < 1) return 1;
@@ -154,14 +156,6 @@ echo ($_smarty_tpl->getValue('serata')['postiLiberi']/$_smarty_tpl->getValue('se
     btnPlus.addEventListener('click', function () {
         input.value = clamp(parseInt(input.value, 10) + 1);
     });
-
-    if (btnPrenota) {
-        btnPrenota.addEventListener('click', function () {
-            const posti = parseInt(input.value, 10);
-            // TODO: chiamata fetch al Controller per la prenotazione
-            console.log('Prenotazione serata', btnPrenota.dataset.id, 'posti:', posti);
-        });
-    }
 })();
 <?php echo '</script'; ?>
 >
