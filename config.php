a5f602ae9a8773f573d406cbc73d8fbef122f5e4
laurabadia22 <laurabadia22@gmail.com>
2026-07-22 20:31:27 +0200

    aggiunte alternative per xampp

--- config.php
+++ config.php
@@ -8,6 +8,9 @@
 // ── AMBIENTE ────────────────────────────────────────────────
 // Cambia solo questa riga quando passi a produzione:
 // 'development' → 'production'
+
+//CODICE PER TEST SU XAMPP
+//define('ENVIRONMENT', 'development');
 define('ENVIRONMENT', 'production'); //development
 
 // ── BASE URL ─────────────────────────────────────────────────
@@ -26,6 +29,19 @@ define('JS_PATH',    ASSETS_URL . '/js');
 define('IMG_PATH',   ASSETS_URL . '/img');
 define('PRESENTATION_PATH', __DIR__ . '/Presentation');
 define('SMARTY_DIR',        PRESENTATION_PATH . '/smarty-dir/');
+
+
+//-----CODCE PER TEST SU XAMPP
+/*
+define('ROOT_PATH',         __DIR__);
+define('PUBLIC_PATH',       __DIR__ . '/public');
+define('ASSETS_URL', BASE_URL . '/public');
+define('CSS_PATH',   PUBLIC_PATH . '/css');
+define('JS_PATH',    PUBLIC_PATH . '/js');
+define('IMG_PATH',   PUBLIC_PATH . '/img');
+define('PRESENTATION_PATH', __DIR__ . '/Presentation');
+define('SMARTY_DIR',        PRESENTATION_PATH . '/smarty-dir/');
+ */
 
 // ── DATABASE ─────────────────────────────────────────────────
 if (ENVIRONMENT === 'development') {
